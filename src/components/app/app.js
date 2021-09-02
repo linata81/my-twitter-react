a38cc160639d2ab4f60ffe7f5a8cbae5a53187e6
linata81 <linata81@gmail.com>
2021-09-02 18:50:19 +0300

    first commit

---
+++ src/components/app/app.js
@@ -0,0 +1,119 @@
+import React, { Component } from 'react';
+
+import AppHeader from '../app-header'
+import SearchPanel from '../search-panel'
+import PostStatusFilter from '../post-status-filter'
+import PostList from '../post-list';
+import PostAddForm from '../post-add-form';
+
+import './app.css';
+import styled from 'styled-components';
+
+const AppBlock = styled.div`
+  margin: 0 auto;
+  max-width: 800px;
+`;
+
+// const StyledAppBlock = styled(AppBlock)`  наследование css стилей
+//   background-color: grey;
+// `;
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        { label: 'Going to learn React', important: true, like: false, id: 1 },
+        { label: 'That is so good', important: false, like: false, id: 2 },
+        { label: 'I need a break...', important: false, like: false, id: 3 }
+      ]
+    };
+    this.deleteItem = this.deleteItem.bind(this);
+    this.addItem = this.addItem.bind(this);
+    this.onToggleImportant = this.onToggleImportant.bind(this);
+    this.onToggleLiked = this.onToggleLiked.bind(this);
+
+    this.maxId = 4;
+  }
+
+  deleteItem(id) {
+    this.setState(({ data }) => {
+      const index = data.findIndex(elem => elem.id === id);
+
+      const newArr = [...data.slice(0, index), ...data.slice(index + 1)];
+      return {
+        data: newArr
+      }
+    });
+  }
+
+  addItem(body) {
+    const newItem = {
+      label: body,
+      important: false,
+      id: this.maxId++
+    }
+    this.setState(({ data }) => {
+      const newArr = [...data, newItem];
+      return {
+        data: newArr
+      }
+    })
+  }
+
+  onToggleImportant(id) {
+    this.setState(({ data }) => {
+      const index = data.findIndex(elem => elem.id === id);
+
+      const old = data[index];
+      const newItem = { ...old, important: !old.important };
+
+      const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
+
+      return {
+        data: newArr
+      }
+    })
+  }
+
+  onToggleLiked(id) {
+    this.setState(({ data }) => {
+      const index = data.findIndex(elem => elem.id === id);
+
+      const old = data[index];
+      const newItem = { ...old, like: !old.like };
+
+      const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
+
+      return {
+        data: newArr
+      }
+    })
+  }
+
+  render() {
+    const { data } = this.state;
+
+    const liked = data.filter(item => item.like).length;
+    const allPosts = data.length;
+
+    return (
+      <AppBlock>
+        <AppHeader
+          liked={liked}
+          allPosts={allPosts} />
+        <div className="search-panel d-flex">
+          <SearchPanel />
+          <PostStatusFilter />
+        </div>
+        <PostList
+          posts={this.state.data}
+          onDelete={this.deleteItem}
+          onToggleImportant={this.onToggleImportant}
+          onToggleLiked={this.onToggleLiked} />
+        <PostAddForm
+          onAdd={this.addItem} />
+      </AppBlock>
+    )
+  }
+}
---
+++ src/components/post-list/post-list.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import PostListItem from '../post-list-item';
+import { ListGroup } from 'reactstrap';
+
+import './post-list.css';
+
+const PostList = ({ posts, onDelete, onToggleImportant, onToggleLiked }) => {
+
+  const elements = posts.map((item) => {
+    const { id, ...itemProps } = item;
+    return (
+      <li key={id} className='list-group-item'>
+        <PostListItem
+          {...itemProps}
+          onDelete={() => onDelete(id)}
+          onToggleImportant={() => onToggleImportant(id)}
+          onToggleLiked={() => onToggleLiked(id)} />
+        {/* <PostListItem        полный вариант записи, выше упростили,  используя spread-оператор
+          label={item.label}
+          important={item.important} /> */}
+      </li>
+    )
+  });
+
+  return (
+    <ListGroup className="app-list">
+      {elements}
+    </ListGroup>
+  )
+}
+
+export default PostList;
